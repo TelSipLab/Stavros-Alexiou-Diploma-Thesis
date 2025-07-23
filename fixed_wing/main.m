b4e46a24abc7a51567e62c3b7e87e0e9cceab26a
StavrosAlx <stavritoalexiou@gmail.com>
2025-07-23 15:40:45 +0300

    better plots

--- fixed_wing/main.m
+++ fixed_wing/main.m
@@ -22,8 +22,8 @@ s0 = [x0; y0; h0; Vg0; gamma0; psi0];
 
 % Double integrator control variables
 ax = 0.1;
-ay = 0.05;
-ah = 0.0;
+ay = 0;
+ah = 0.05;
 control_vars = [ax ay ah];
 
 % ODE
@@ -35,19 +35,71 @@ Tspan = [0 simulation_time];
 x = S(:, 1);
 y = S(:, 2);
 h = S(:, 3);
+Vg = S(:, 4);
+gamma = S(:, 5);
+psi = S(:, 6);
+
+%% PLOTS
 
 % trajectory plot
 figure;
 plot3(x, y, h, 'b-', 'LineWidth', 1.5); hold on;
 
 % initial & final drone position
-plot3(x(1), y(1), h(1), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'g');
-plot3(x(end), y(end), h(end), 'go', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
+plot3(x(1), y(1), h(1), 'bo', 'MarkerSize', 10, 'MarkerFaceColor', 'g');
+plot3(x(end), y(end), h(end), 'bo', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
 
-% labels
+% trajectory labels
 xlabel('x [m]');
 ylabel('y [m]');
 zlabel('h [m]');
 title('UAV 3D Trajectory');
 grid on;
 legend('Drone Trajectory', 'Initial Position', 'Final Position');
+
+figure;
+
+% x
+subplot(6,1,1);
+plot(t, S(:,1), 'b', 'LineWidth', 1.5);
+grid on;
+ylabel('x [m]');
+title('Position x');
+
+% y
+subplot(6,1,2);
+plot(t, S(:,2), 'b', 'LineWidth', 1.5);
+grid on;
+ylabel('y [m]');
+title('Position y');
+
+% h
+subplot(6,1,3);
+plot(t, S(:,3), 'b', 'LineWidth', 1.5);
+grid on;
+ylabel('h [m]');
+title('Altitude h');
+
+% Vg
+subplot(6,1,4);
+plot(t, S(:,4), 'c', 'LineWidth', 1.5);
+grid on;
+ylabel('V_g [m/s]');
+title('Ground Speed');
+
+% gamma 
+subplot(6,1,5);
+plot(t, rad2deg(S(:,5)), 'm', 'LineWidth', 1.5);
+grid on;
+ylabel('\gamma [deg]');
+title('Flight Path Angle');
+
+% psi
+subplot(6,1,6);
+plot(t, rad2deg(S(:,6)), 'm', 'LineWidth', 1.5);
+grid on;
+xlabel('Time [s]');
+ylabel('\psi [deg]');
+title('Heading Angle');
+
+sgtitle('UAV State Evolution');
